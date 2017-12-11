b6367d194d52d40001290b9edbe02d0dc34eae5d
Summer-Qing <16210720158@fudan.edu.cn>
2017-12-11 11:28:07 +0800

    Optize view

--- test.py
+++ test.py
@@ -3,14 +3,15 @@ import os
 
 output_dic ={}
 output_dic_ls = []
-for i in range(12):
+for i in range(7000):
     accur = os.popen(' python tf_emg_mnist.py {}'.format(i))
-    info =  "i: ", i, "accur: ", accur.read()
-    output_dic_ls.append(info)
+    info =  "i:{} | accur: {} ".format(i, accur.read())
+    print info
     output_file = open('Test.log', 'a+')
+    output_dic_ls.append(info)
     output_file.writelines('{}'.format(info))
-    output_file.close()
     output_dic['{}'.format(i)] = '{}'.format(accur.read())
+    output_file.close()
 output_file1 = open('output_dic.log', 'a+') 
 output_file1.writelines('{}'.format(output_dic))
 output_file1.writelines('{}'.format(output_dic_ls))
